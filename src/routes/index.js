5c57c559501d5825f385dfcf09d046a3da3c3a83
kanekotic <alvarojosepl@gmail.com>
2018-07-21 20:07:49 +0300

    simplified logic

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,32 +2,14 @@ const router = require('express').Router()
 var AppError = require('express-exception-handler').exception
 
 let something = []
-let id = 1
 
 router.post('/garbage', async (req, res) => {
-    let formatedData = {
-        id: id,
-        location: {
-            lat: 1.2,
-            long: 2.6
-        },
-    }
-    id += 1
-    something.push(formatedData)
-    res.send(something)
+    something.push(req.body)
+    res.send(req.body)
 })
 
 router.put('/garbage/:id', async (req, res) => {
-    let formatedData = {
-        id: id,
-        location: {
-            lat: 1.2,
-            long: 2.6
-        },
-    }
-    id += 1
-    something.push(formatedData)
-    res.send(something)
+    res.send(req.body)
 })
 
 router.get('/garbage', async (req, res) => {
