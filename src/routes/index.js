717a9c6cd006092a0163bb8a24b83ccb0325420e
kanekotic <alvarojosepl@gmail.com>
2018-07-21 16:18:04 +0300

    added put example

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,37 @@
 const router = require('express').Router()
 var AppError = require('express-exception-handler').exception
 
-router.get('/', async (req, res) => {
-    res.send("hello cleanly")
+let something = []
+let id = 1
+
+router.post('/garbage', async (req, res) => {
+    let formatedData = {
+        id: id,
+        location: {
+            lat: 1.2,
+            long: 2.6
+        },
+    }
+    id += 1
+    something.push(formatedData)
+    res.send(something)
 })
 
-router.get('/error', async (req, res) => {
-        throw new AppError('This is to test error', 400)
+router.put('/garbage/:id', async (req, res) => {
+    let formatedData = {
+        id: id,
+        location: {
+            lat: 1.2,
+            long: 2.6
+        },
+    }
+    id += 1
+    something.push(formatedData)
+    res.send(something)
+})
+
+router.get('/garbage', async (req, res) => {
+    res.send(something)
 })
 
 module.exports = router
