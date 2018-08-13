77a8df612309dc7418a31d2b56374f73e9c51b2b
kanekotic <alvarojosepl@gmail.com>
2018-08-13 23:22:50 +0200

    added initialization code for mongoose

--- src/adapters/item.js
+++ src/adapters/item.js
@@ -1,2 +1,25 @@
-const save = () => {}
-module.exports = { save }
+const db = require('mongoose'),
+    connectionString = 'mongodb://localhost/my_database'
+
+const models = {
+    item: undefined
+}
+
+const initialize = () => {
+    db.connect(connectionString)
+    models.item = new db.Schema({ 
+        location: { 
+            type: String, 
+            coordinates: [Number] 
+        },
+        tags: [String]
+    })
+}
+
+const save = (data) => {}
+
+module.exports = { 
+    save, 
+    initialize,
+    models
+}
--- test/unit/adapters/item-test.js
+++ test/unit/adapters/item-test.js
@@ -1,7 +1,41 @@
-const item = require('../../../src/adapters/item')
+jest.mock('mongoose', () =>({
+    connect: jest.fn(),
+    model: jest.fn(),
+    Schema: jest.fn()
+}))
+
+const item = require('../../../src/adapters/item'),
+    db = require('mongoose'),
+    faker = require('faker')
+
 describe('item adapter should', () => {
-    test('should implement save an item in db', async () => {
-        let data = {}
-        item.save(data)
+
+    beforeEach(() => {
+        db.connect.mockClear()
+        db.model.mockClear()
+    })
+
+    test('initialize should initialize connection', async () => {
+        let connection = 'mongodb://localhost/my_database'
+        
+        item.initialize()
+
+        expect(db.connect).toBeCalledWith(connection)
+    })
+
+    test('initialize should export item', async () => {
+        let itemSchema = {some: faker.random.uuid()}
+        db.Schema.mockImplementation(() => itemSchema)
+        
+        item.initialize()
+
+        expect(db.Schema).toBeCalledWith({ 
+            location: { 
+                type: String, 
+                coordinates: [Number] 
+            },
+            tags: [String]
+        })
+        expect(item.models.item).toEqual(itemSchema)
     })
 })
